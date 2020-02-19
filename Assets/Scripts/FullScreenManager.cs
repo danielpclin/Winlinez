fd1aaad19d8ab1b6c09c0e571f56d1727ab5ec57
danielpclin <daniel.pclin@gmail.com>
2020-02-20 06:19:17 +0800

    add icon add fullscreen management

---
+++ Assets/Scripts/FullScreenManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Screen.fullScreen = false;
+        Screen.SetResolution(1024, 768, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
